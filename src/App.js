ccde8d825ffbf496db27a1aa05ad716a369e11aa
Kurt Harris <kurtharris1541@gmail.com>
2024-10-05 13:49:47 +0000

    installed react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
